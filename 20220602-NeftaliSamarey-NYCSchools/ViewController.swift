7590861153604a4fc057eb09de8072db64596166
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2022-06-02 02:03:56 -0400

    Initial Commit

---
+++ 20220602-NeftaliSamarey-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20220602-NeftaliSamarey-NYCSchools
+//
+//  Created by Neftali Samarey on 6/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
