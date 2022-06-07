9bbef05ae50a1bd27c1ca37294d40eae9164ed0d
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2022-06-07 03:06:31 -0400

    Coordinators configured for detail presentation controller

---
+++ 20220602-NeftaliSamarey-NYCSchools/Screens/Home/Coordinators/Views/CardController/Coordinators/SchoolDetailCoordinator.swift
@@ -0,0 +1,12 @@
+//
+//  SchoolDetailCoordinator.swift
+//  20220602-NeftaliSamarey-NYCSchools
+//
+//  Created by Neftali Samarey on 6/7/22.
+//
+
+import Foundation
+
+class SchoolDetailCoordinator {
+    
+}
---
+++ 20220602-NeftaliSamarey-NYCSchools/Screens/Home/Coordinators/Views/CardController/Coordinators/SchoolDetailViewCoordinator.swift
@@ -0,0 +1,12 @@
+//
+//  SchoolDetailViewCoordinator.swift
+//  20220602-NeftaliSamarey-NYCSchools
+//
+//  Created by Neftali Samarey on 6/7/22.
+//
+
+import Foundation
+
+class SchoolDetailViewCoordinator {
+    //
+}
