3c058a0f4616bb47d763337cc629c40d518c7322
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2022-06-07 01:47:02 -0400

    Schools data structure model updated with the rest of needed properties

--- 20220602-NeftaliSamarey-NYCSchools/Networking/Models/Schools.swift
+++ 20220602-NeftaliSamarey-NYCSchools/Networking/Models/Schools.swift
@@ -10,4 +10,35 @@ import Foundation
 public struct Schools: Codable {
     let dbn: String
     let school_name: String
+    let boro: String
+    let overview_paragraph: String
+    let school_10th_seats: String
+    let academicopportunities1: String
+    let academicopportunities2: String
+    let academicopportunities3: String
+    let academicopportunities4: String
+    let ell_programs: String
+    let language_classes: String
+    let advancedplacement_courses: String
+    let neighborhood: String
+    let campus_name: String
+    let building_code: String
+    let location: String
+    let phone_number: String
+    let fax_number: String
+    let school_email: String
+    let website: String
+    let subway: String
+    let bus: String
+    let grades2018: String
+    let finalgrades: String
+    let total_students: String
+    let extracurricular_activities: String
+    let primary_address_line_1: String
+    let city: String
+    let zip: String
+    let state_code: String
+    let latitude: String
+    let longitude: String
+    let borough: String
 }
