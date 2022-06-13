db0321d956a5d447fc204d31b3c86f451af2c747
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2022-06-13 06:00:11 -0400

    Removed redundant network call

--- 20220602-NeftaliSamarey-NYCSchools/Networking/NetworkManager.swift
+++ 20220602-NeftaliSamarey-NYCSchools/Networking/NetworkManager.swift
@@ -9,28 +9,6 @@ import Foundation
 
 class NetworkManager {
 
-    func getSchools(completion: @escaping (Swift.Result<[Schools], Error>) -> ()) {
-
-        let url = URL(string: "https://data.cityofnewyork.us/resource/s3k6-pzi2.json")!
-        let request = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let _data = data else {
-                return
-            }
-
-            do {
-                let decoder = JSONDecoder()
-                let schoolData = try decoder.decode([Schools].self, from: _data)
-
-                completion(.success(schoolData))
-
-            } catch let error {
-                completion(.failure(error))
-            }
-        }
-
-        request.resume()
-    }
-
     // Fetches the general list of schools
     func fetchSchoolListAPI(callback: @escaping ([Schools]) -> Void) {
 
